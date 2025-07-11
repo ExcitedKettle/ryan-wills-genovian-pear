f2a6d59785aa757b0db323fb457ca0aca64fdef0
Ryan <ryan.wills@mytutor.co.uk>
2025-07-11 16:38:12 +0100

    initial

---
+++ src/main/java/org/excited/kettle/ryanwillsgenovianpear/RyanWillsGenovianPearApplication.java
@@ -0,0 +1,13 @@
+package org.excited.kettle.ryanwillsgenovianpear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanWillsGenovianPearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyanWillsGenovianPearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/excited/kettle/ryanwillsgenovianpear/RyanWillsGenovianPearApplicationTests.java
@@ -0,0 +1,13 @@
+package org.excited.kettle.ryanwillsgenovianpear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanWillsGenovianPearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
